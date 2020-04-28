fab057c0e838d0d67be73566570220506f727954
pmalonis <pjmalonis@gmail.com>
2020-04-28 16:07:53 -0400

    full_input_plots now plots normal acceleration and  target appearance times

--- notebooks/full_input_plots.py
+++ notebooks/full_input_plots.py
@@ -15,12 +15,12 @@ used_inds = input_info['validInds'][0] - 1
 df = pd.read_pickle(data_filename)
 
 with h5py.File(lfads_filename) as h5file:
-    dt = np.round(trial_len_ms/h5file['controller_outputs'].shape[1])/1000 
     trial_len_ms = input_info['seq_timeVector'][-1][-1]
+    dt = np.round(trial_len_ms/h5file['controller_outputs'].shape[1])/1000 
     trial_len = trial_len_ms/1000
     trial_len = np.floor(trial_len/dt)*dt
     lfads_t = np.arange(0, trial_len, dt)
-    for trial_idx in used_inds[:10]:
+    for i, trial_idx in enumerate(used_inds):
         plt.figure(figsize=(10,6))
         data_t = df.loc[trial_idx].loc[:trial_len].index.values
         x_vel = df.loc[trial_idx].loc[:trial_len].kinematic['x_vel'].values
@@ -32,14 +32,19 @@ with h5py.File(lfads_filename) as h5file:
         x_jerk = np.gradient(x_accel, data_t)
         y_jerk = np.gradient(y_accel, data_t)
         jerk = np.sqrt(x_accel**2, y_accel**2)
-        plt.plot(data_t, speed, 'g')
-        plt.legend(['speed'])
+        x_tang_velocity = x_vel/speed
+        y_tang_velocity = y_vel/speed
+        normal_accel = np.sqrt(np.gradient(x_tang_velocity, data_t)**2 + np.gradient(y_tang_velocity, data_t)**2)
+        plt.plot(data_t, normal_accel, 'g')
+        plt.legend(['normal acceleration'])
         ax1 = plt.gca()
         ax2 = ax1.twinx()
         n_inputs = h5file['controller_outputs'].shape[2]
         legend=[]
+        targets = df.loc[trial_idx].kinematic.loc[:trial_len].query('hit_target').index.values
+        plt.vlines(targets, -.8,.8)
         for input_idx in range(n_inputs):
-            ax2.plot(lfads_t, h5file['controller_outputs'][trial_idx,:,input_idx])
+            ax2.plot(lfads_t, h5file['controller_outputs'][i,:,input_idx])
             legend.append('input %d'%input_idx)
 
         plt.legend(legend)
