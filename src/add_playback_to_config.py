e1face1814422b530b2d0f1adf96ff3906155c05
pmalonis <pjmalonis@gmail.com>
2020-11-15 21:03:53 -0500

    created add_playback_to_config.py to update config for playback experiment

---
+++ src/add_playback_to_config.py
@@ -0,0 +1,25 @@
+import yaml
+from glob import glob
+import os
+
+if __name__=='__main__':
+    raw_root = '/home/macleanlab/peter/lfads_analysis/data/raw/Playback-NN/split_condition/'
+    params = ['param_kWj--O']
+    lfads_root = '/home/macleanlab/peter/lfads_analysis/data/model_output/playback_first/param_kWJ--O/'
+    server_path = 'macleanlab@205.208.22.226'
+    config_path = os.path.join(os.path.dirname(__file__), '../config.yml')
+    cfg = yaml.safe_load(open(config_path, 'r'))
+    dsets = os.listdir(raw_root)
+    conds = os.listdir(raw_root + dsets[0])
+    conds = [c.split('.')[0] for c in conds]
+    datasets = cfg['datasets']
+    for dset in dsets:
+        for cond in conds:
+            raw_path = raw_root + dset + '/' cond + '.mat'
+            param_dict = {}
+            for param in params:
+                for trial_type in ['valid', 'train']:
+                    param_dict[trial_type] = lfads_root + 'single_' + cond + '/lfadsOutput/model_runs_active.h5_%s_posterior_sample_and_average'%trial_type
+            dset_dict = {'raw':raw_path, param:param_dict}
+
+            datasets['_'.join([dset, cond])] = dset_dict
