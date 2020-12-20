7abfd004464a9ae3cd30dac1e147ea690347d726
pmalonis <pmalonis@uchicago.edu>
2020-12-20 15:07:18 -0600

    adding analysis of gini coefficient of subsampled rockstar trials

---
+++ src/analyze_subsample.py
@@ -0,0 +1,27 @@
+import subsample_analysis as sa
+from glob import glob
+import parse
+import h5py
+import matplotlib.pyplot as plt
+from importlib import reload
+reload(sa)
+
+if __name__=='__main__':
+    filenames = glob('../data/model_output/rockstar_*_trial_*_valid.h5')
+    mean_1 = []
+    mean_2 = []
+    ntrials = []
+    for filename in filenames:
+        parse_result = parse.parse('{}rockstar_{ntrials:d}_trial_{}_all.h5', filename)
+        ntrials.append(parse_result['ntrials'])
+        with h5py.File(filename) as h5file:
+            co = h5file['controller_output'].values
+
+        mean_1.append(np.mean([sa.gini(co[i, :, 0]) for i in range(co.shape[0])]))
+        mean_2.append(np.mean([sa.gini(co[i, :, 1]) for i in range(co.shape[0])]))
+
+    plt.plot(ntrials, mean_1)
+    plt.plot(ntrials, mean_2)
+    plt.xlabel('Number of trials')
+    plt.ylabel('Gini coefficient')
+    plt.legend(['Input 1', 'Input 2'])
