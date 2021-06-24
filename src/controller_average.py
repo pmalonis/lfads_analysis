c2b246f832f72343bd25827ea14d4e847c0e315e
macleanlab <joshcruz428@gmail.com>
2021-06-24 12:25:09 -0500

    adding notebooks

--- src/controller_average.py
+++ src/controller_average.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import optimize_target_prediction as opt
+import feedback_optimize_target_prediction as fotp
 import utils
 from importlib import reload
 import os
@@ -10,9 +11,9 @@ import yaml
 from scipy import io
 import h5py
 from matplotlib import rcParams
+from scipy.signal import savgol_filter
 plt.rcParams['axes.spines.top'] = False
 plt.rcParams['axes.spines.right'] = False
-plt.rcParams['font.family'] = 'FreeSarif'
 reload(opt)
 reload(utils)
 
@@ -20,18 +21,21 @@ reload(utils)
 config_path = os.path.join(os.path.dirname(__file__), '../config.yml')
 cfg = yaml.safe_load(open(config_path, 'r'))
 
-datasets = ['rockstar','raju', 'mack']
-params = ['final-fixed-2OLS24', 'final-fixed-2OLS24', 'mack-kl-co-sweep-0Wo8i9']
+run_info = yaml.safe_load(open('../lfads_file_locations.yml', 'r'))
+datasets = list(run_info.keys())
+params = []
+for dataset in datasets:
+    params.append(open('../data/peaks/%s_selected_param_spectral.txt'%(dataset)).read())
+
 nbins = 12
-win_start = cfg['post_target_win_start']
-win_stop = cfg['post_target_win_stop']
+win_start = 0#-0.1#-0.1#cfg['post_target_win_start']
+win_stop = 0.2#0.1#cfg['post_target_win_stop']
 
 lfads_filename = '../data/model_output/' + '_'.join([datasets[0], params[0], 'all.h5'])
 with h5py.File(lfads_filename, 'r+') as h5file:
     co = h5file['controller_outputs'][:]
     
 n_co = co.shape[2]
-
 if __name__=='__main__':
     fig1, axplot = plt.subplots(n_co, len(datasets))
     for i, (dataset, param) in enumerate(zip(datasets, params)):
@@ -46,6 +50,7 @@ if __name__=='__main__':
             dt = utils.get_dt(h5file, input_info)
             trial_len = utils.get_trial_len(h5file, input_info)
 
+        co = savgol_filter(co, 11, 2, axis=1)
         peak_df_train = pd.read_pickle('../data/peaks/%s_%s_peaks_train.p'%(dataset,param))
         peak_df_test = pd.read_pickle('../data/peaks/%s_%s_peaks_test.p'%(dataset,param))
 
@@ -53,18 +58,19 @@ if __name__=='__main__':
 
         X,y = opt.get_inputs_to_model(peak_df, co, trial_len, dt, 
                                     win_start=win_start, 
-                                    win_stop=win_stop)
+                                    win_stop=win_stop)#, align_peaks=True, align_win_start=0.0, align_win_stop=0.2)
 
         theta = np.arctan2(y[:,1], y[:,0])
         assert(nbins%2==0)
         
         bin_theta = np.pi / (nbins/2)
         colors = sns.color_palette('hls', nbins)
-        t_ms = np.arange(win_start, win_stop, dt) * 1000
+        t_ms = range(int(win_start/dt), int(win_stop/dt)) * dt * 1000
+        win_size = int((win_stop - win_start)/dt)
         for j in range(n_co):
             fig, axplot = plt.subplots()
             axplot.set_title('Controller %s'%(j+1))
-            ymin, ymax = (np.min(X[:,j::n_co]), np.max(X[:,j::n_co]))
+            ymin, ymax = (np.min(X[:,j*win_size:(j+1)*win_size]), np.max(X[:,j*win_size:(j+1)*win_size]))
             
             #setting dimensions for inset
             left = .7#win_start + .8 * (win_stop-win_start)
@@ -77,7 +83,7 @@ if __name__=='__main__':
             for i in range(-nbins//2, nbins//2):
                 min_theta = i * bin_theta
                 max_theta = (i+1) * bin_theta
-                co_av = X[:,j::n_co][(theta > min_theta) & (theta <= max_theta)].mean(0)
+                co_av = X[:,j*win_size:(j+1)*win_size][(theta > min_theta) & (theta <= max_theta)].mean(0)
                 
                 color = colors[i+nbins//2]
                 axplot.plot(t_ms, co_av, color=color)
@@ -90,33 +96,33 @@ if __name__=='__main__':
                     co_argmax = i
                     co_max = np.max(co_av)
 
-            fig2, extremeplot = plt.subplots()
-            inset_ex = fig2.add_axes([left, bottom, width, height], polar=True)
-            for i in [co_argmin, co_argmax]:        
-                min_theta = i * bin_theta
-                max_theta = (i+1) * bin_theta
-                x_binned = X[:,j::n_co][(theta > min_theta) & (theta <= max_theta)]
-                co_av = x_binned.mean(0)
-                error = x_binned.std(0)#/x_binned.shape[0]
-                color = colors[i+nbins//2]
-                extremeplot.plot(t_ms, co_av, color=color)
-                extremeplot.fill_between(t_ms, co_av-error,co_av+error, alpha=0.3, color=color)
-                inset_ex.hist([(i+0.5) * bin_theta], [min_theta, max_theta], color=color)
+            # fig2, extremeplot = plt.subplots()
+            # inset_ex = fig2.add_axes([left, bottom, width, height], polar=True)
+            # for i in [co_argmin, co_argmax]:        
+            #     min_theta = i * bin_theta
+            #     max_theta = (i+1) * bin_theta
+            #     x_binned = X[:,j*win_size:(j+1)*win_size][(theta > min_theta) & (theta <= max_theta)]
+            #     co_av = x_binned.mean(0)
+            #     error = x_binned.std(0)#/x_binned.shape[0]
+            #     color = colors[i+nbins//2]
+            #     extremeplot.plot(t_ms, co_av, color=color)
+            #     extremeplot.fill_between(t_ms, co_av-error,co_av+error, alpha=0.3, color=color)
+            #     inset_ex.hist([(i+0.5) * bin_theta], [min_theta, max_theta], color=color)
 
 
             axplot.set_xlabel('Time from target appearance (ms)')
             axplot.set_ylabel('Controller Value (a.u.)')
-            axplot.set_title('Monkey %s'%dataset)
+            axplot.set_title('Monkey %s Controller %d'%(dataset, j+1))
             inset.set_rticks([])
             inset.set_xticks([])
             inset.set_yticks([])
             inset.set_title('Target Direction', fontdict={'fontsize': 8})
             inset.spines['polar'].set_visible(False)
-            inset_ex.set_rticks([])
-            inset_ex.set_xticks([])
-            inset_ex.set_yticks([])
-            inset_ex.set_title('Target Direction', fontdict={'fontsize': 8})
+            # inset_ex.set_rticks([])
+            # inset_ex.set_xticks([])
+            # inset_ex.set_yticks([])
+            # inset_ex.set_title('Target Direction', fontdict={'fontsize': 8})
             #axplot.set_ylim([ymin, ymax])
-
+            fig.savefig('/home/macleanlab/peter/rockstar_ctrl_%d.png'%(j+1))
             
                 
